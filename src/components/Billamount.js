a61adc0f263b57a3c1199b443f246c867fcb74ae
Partha Pratim Sarma <parthasarma34@gmail.com>
2021-07-19 15:40:43 +0530

    Basic file setup

---
+++ src/components/Billamount.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Billamount() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Billamount
---
+++ src/components/ReturnChange.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ReturnChange() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ReturnChange
